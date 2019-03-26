91319581e9f9493acfe8688db92b12c509fa7fc6
ITsHyeon <suhyeon917917@gmail.com>
2019-03-26 12:53:06 +0900

    firebase connect

--- app/src/main/java/com/example/gongumi/LoginActivity.java
+++ app/src/main/java/com/example/gongumi/LoginActivity.java
@@ -2,14 +2,20 @@ package com.example.gongumi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 액션바 없애기
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_login);
 
-        // TODO : 로그인 Activity
+        // TODO : 로그인 Activity, splash 효과, 상단바 없애기
     }
 }
