44d1bb0af4ad90d3a74e1f80e2906029fa7ec6c8
Suhyeon Lee <suhyeon917917@gmail.com>
2019-04-16 13:15:22 +0900

    init setting

--- app/src/main/java/com/example/gongumi/activity/SettingActivity.java
+++ app/src/main/java/com/example/gongumi/activity/SettingActivity.java
@@ -2,17 +2,31 @@ package com.example.gongumi.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.gongumi.R;
 
-public class SettingActivity extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
 
+public class SettingActivity extends AppCompatActivity {
+    private TextView mTvName, mTvAdress;
+    private LinearLayout mLiPostList, mLiJoinList, mLiLogOut;
+    private CircleImageView mCiChangeProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
         // TODO : 설정 Activity
+        mTvName = findViewById(R.id.tvName);
+        mTvAdress = findViewById(R.id.tvAdress);
+        mLiPostList = findViewById(R.id.liPostList);
+        mLiJoinList = findViewById(R.id.liJoinList);
+        mLiLogOut = findViewById(R.id.liLogOut);
+        mCiChangeProfile = findViewById(R.id.ciChangeProfile);
+
+
 
     }
 }
